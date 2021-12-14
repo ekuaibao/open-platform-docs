97befce578158da8bc700ab6dcba2b7b88101173
zhangping <zhangping@hosecloud.com>
2021-12-14 14:46:09 +0800

    fix edit urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,13 +26,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: "https://github.com/facebook/docusaurus/edit/main/website/",
+          editUrl: "https://github.com/ekuaibao/open-platform-docs/docs/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            "https://github.com/facebook/docusaurus/edit/main/website/blog/",
+            "https://github.com/ekuaibao/open-platform-docs/blogs/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
